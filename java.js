d67f43b162770bdce5ef11f81eaa5736e4eaaf79
SarayutZ <wanoizloserBZ@gmail.com>
2023-08-15 08:32:13 +0700

    update1

---
+++ java.js
@@ -0,0 +1,6 @@
+// // JavaScript
+var screenWidth = window.innerWidth;
+var screenHeight = window.innerHeight;
+
+console.log("ความกว้างของหน้าจอ: " + screenWidth + "px");
+console.log("ความสูงของหน้าจอ: " + screenHeight + "px");
